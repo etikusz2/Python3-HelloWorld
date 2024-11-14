4771ee29f3da37fea0e830e44e8e11d70e242e0f
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-14 17:48:27 +0200

    refactoring

--- escapechar.py
+++ escapechar.py
@@ -1,8 +1,8 @@
-spliteString = "This string has been\nsplit over\nseveral\nlines"
-print(spliteString)
+split_string = "This string has been\nsplit over\nseveral\nlines"
+print(split_string)
 
-tabledString = "1\t2\t3\t4\t5"
-print(tabledString)
+tabled_string = "1\t2\t3\t4\t5"
+print(tabled_string)
 
 print('The pet shop owner said "no, no,  \'e\'s uh,...he\'s resting".')
 # or
@@ -10,12 +10,12 @@ print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 # or
 print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
 
-anotherSplitString = """This string has been \
+another_split_string = """This string has been \
 split over \
 several \
 lines"""
 
-print(anotherSplitString)
+print(another_split_string)
 
 print("C:\\Users\\tim\\notes.txt")
 print(r"C:\Users\tim\notes.txt")
